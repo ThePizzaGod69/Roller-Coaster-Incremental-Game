5c0d25a146a3359bd0301d9e235831b08742f71d
ThePizzaGod69 <skeltonstevie@gmail.com>
2024-11-05 10:19:30 -0500

    Update inversions.js

--- inversions-page/inversions.js
+++ inversions-page/inversions.js
@@ -1,6 +1,6 @@
 let inversionBuyable1 = {
     text1: "Press this to get 1 more meter of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+    text2: " inversions<br>Bought: ",
     count: 0,
     startCost: 1,
     cost: 1,
@@ -9,52 +9,63 @@ let inversionBuyable1 = {
 };
 
 let inversionBuyable2 = {
-    text1: "Press this to get 2 more meters of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+    text1: "Roll<br>(makes rider gain ^1.1)<br>Cost: ",
+    text2: " inversions<br>Bought: ",
     count: 0,
-    startCost: 4,
-    cost: 4,
+    startCost: 1,
+    cost: 1,
     exponent: 1.2,
     button: $("#inversion2")
 };
 
 let inversionBuyable3 = {
-    text1: "Press this to get 5 more meters of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+    text1: "Vertical Loop<br>
+          Cost: 1 inversion<br>
+          makes hill gain ^1.05",
+    text2: " inversions<br>Bought: ",
     count: 0,
-    startCost: 10,
-    cost: 10,
-    exponent: ,
+    startCost: 1,
+    cost: 1,
+    exponent: 1.4,
     button: $("#inversion3")
 };
 
 let inversionBuyable4 = {
-    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+    text1: "Dive Loop<br>(rider gain *6.9)<br>Cost: ",
+    text2: " inversions<br>Bought: ",
     count: 0,
-    startCost: 20,
-    cost: 20,
-    exponent: 2.5,
+    startCost: 5,
+    cost: 5,
+    exponent: 1.7,
     button: $("#inversion4")
 };
-let inversionBuyable4 = {
-    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+let inversionBuyable5 = {
+    text1: "Batwing<br>(hill gain *10)<br>Cost: ",
+    text2: " inversions<br>Bought: ",
     count: 0,
-    startCost: 20,
-    cost: 20,
-    exponent: 2.5,
-    button: $("#inversion4")
+    startCost: 10,
+    cost: 10,
+    exponent: 2,
+    button: $("#inversion5")
 };
 let inversionBuyable6 = {
-    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
-    text2: " riders<br>Bought: ",
+    text1: "Pretzel Knot<br>(rider gain^1.5)<br>Cost: ",
+    text2: "inversions<br>Bought: ",
     count: 0,
-    startCost: 20,
-    cost: 20,
+    startCost: 25,
+    cost: 25,
     exponent: 2.5,
-    button: $("#inversion4")
+    button: $("#inversion6")
 };
 let inversionUpgrade = {
     text:"Press here to unlock themes<br>Requires 100 inversions",
 }
+Corkscrew<br>
+Cost: 1 inversion<br>
+gives*5 rider gain</button>
+    <!--Lets you boost hill gain-->
+        <button id="inversion2">Vertical Loop<br>
+          Cost: 1 inversion<br>
+          makes hill gain ^1.05
+          <!--Lets you make a vertical loop a hill gain.-->
+
