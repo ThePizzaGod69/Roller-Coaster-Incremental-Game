36c316f5f65c232a0ab6d367f6b87d90dda2afe3
ThePizzaGod69 <skeltonstevie@gmail.com>
2024-11-05 10:08:39 -0500

    Update inversions.js

---
+++ inversions-page/inversions.js
@@ -0,0 +1,60 @@
+let inversionBuyable1 = {
+    text1: "Press this to get 1 more meter of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 1,
+    cost: 1,
+    exponent: 1.1,
+    button: $("#inversion1")
+};
+
+let inversionBuyable2 = {
+    text1: "Press this to get 2 more meters of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 4,
+    cost: 4,
+    exponent: 1.2,
+    button: $("#inversion2")
+};
+
+let inversionBuyable3 = {
+    text1: "Press this to get 5 more meters of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 10,
+    cost: 10,
+    exponent: ,
+    button: $("#inversion3")
+};
+
+let inversionBuyable4 = {
+    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 20,
+    cost: 20,
+    exponent: 2.5,
+    button: $("#inversion4")
+};
+let inversionBuyable4 = {
+    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 20,
+    cost: 20,
+    exponent: 2.5,
+    button: $("#inversion4")
+};
+let inversionBuyable6 = {
+    text1: "Press this to get 10 more meters of inversion<br>Requires: ",
+    text2: " riders<br>Bought: ",
+    count: 0,
+    startCost: 20,
+    cost: 20,
+    exponent: 2.5,
+    button: $("#inversion4")
+};
+let inversionUpgrade = {
+    text:"Press here to unlock themes<br>Requires 100 inversions",
+}
