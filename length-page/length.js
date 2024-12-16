8fbb6cae7df44d6d7593224e9d78a18a28dc592f
ThePizzaGod69 <skeltonstevie@gmail.com>
2024-12-16 12:26:47 -0500

    hope

--- length-page/length.js
+++ length-page/length.js
@@ -70,7 +70,7 @@ function updateLengthBuyables() {
 
 // Function to handle buying a length buyable (1 meter)
 function buyLengthBuyable1() {
-    if (gameData.riders.gt(lengthBuyable1.cost)    ) {
+    if (gameData.riders.gt(lengthBuyable1.cost)==true) {
         gameData.riders = gameData.riders.sub(lengthBuyable1.cost); // Take away riders
         lengthBuyable1.count = lengthBuyable1.count.plus(new Decimal(1)); // Increment count
         gameData.length = gameData.length.plus(new Decimal(1)); // Increase length by 1 meter
@@ -84,7 +84,7 @@ function buyLengthBuyable1() {
 
 // Function to handle buying a length buyable (2 meters)
 function buyLengthBuyable2() {
-    if (gameData.riders >= lengthBuyable2.cost) {
+    if (gameData.riders.gt(lengthBuyable2.cost)==true) {
         gameData.riders = gameData.riders.sub(lengthBuyable2.cost); // Take away riders
         lengthBuyable2.count = lengthBuyable2.count.plus(new Decimal(1)); // Increment count
         gameData.length = gameData.length.plus(new Decimal(2)); // Increase length by 2 meters
@@ -98,7 +98,7 @@ function buyLengthBuyable2() {
 
 // Function to handle buying a length buyable (5 meters)
 function buyLengthBuyable3() {
-    if (gameData.riders >= lengthBuyable3.cost) {
+    if (gameData.riders .gt(lengthBuyable3.cost)==true) {
         gameData.riders = gameData.riders.sub(lengthBuyable3.cost); // Take away riders
         lengthBuyable3.count = lengthBuyable3.count.plus(new Decimal(1)); // Increment count
         gameData.length = gameData.length.plus(new Decimal(5)); // Increase length by 5 meters
@@ -112,7 +112,7 @@ function buyLengthBuyable3() {
 
 // Function to handle buying a length buyable (10 meters)
 function buyLengthBuyable4() {
-    if (gameData.riders >= lengthBuyable4.cost) {
+    if (gameData.riders .gt(lengthBuyable4.cost)==true) {
         gameData.riders = gameData.riders.sub(lengthBuyable4.cost); // Take away riders
         lengthBuyable4.count = lengthBuyable4.count.plus(new Decimal(1)); // Increment count
         gameData.length = gameData.length.plus(new Decimal(10)); // Increase length by 10 meters
@@ -124,7 +124,7 @@ function buyLengthBuyable4() {
     }
 }
 function buyLengthUpgrade(){
-    if(gameData.length>=250){
+    if(gameData.length.gt(250)==true){
         if(gameData.lengthUpgrade==false){
             gameData.lengthUpgrade=true;
 
