d9e668b7ce2d1e4d0de3fce949865eef0a5261f6
ThePizzaGod69 <skeltonstevie@gmail.com>
2025-01-31 12:07:54 -0500

    ea

--- length-page/length.js
+++ length-page/length.js
@@ -1,22 +1,20 @@
-loadGame();
-
+deleteSave()
 // Function to update the length of the coaster
 function updateLength() {
     $("#lengthText").text("Your coaster is " + lengthArray[0].toString() + " meters long");
 }
-
 // Function to update the length buyable buttons with their current cost and count
 function updateLengthBuyables() {
-    while(i<4){
-    i+=1;
-    thing="#length"+i;
-    $("#length1").html(lengthArray[i].text1 + lengthArray[i].cost.toString() + lengthArray[i].text2 + lengthArray[i].count.toString());
-    }
+    $("#length1").html(lengthArray[1].text1 + lengthArray[1].cost.toString() + lengthArray[1].text2 + lengthArray[1].count.toString());
+    $("#length2").html(lengthArray[2].text1 + lengthArray[2].cost.toString() + lengthArray[2].text2 + lengthArray[2].count.toString());
+    $("#length3").html(lengthArray[3].text1 + lengthArray[3].cost.toString() + lengthArray[3].text2 + lengthArray[3].count.toString());
+    $("#length4").html(lengthArray[4].text1 + lengthArray[4].cost.toString() + lengthArray[4].text2 + lengthArray[4].count.toString());
     if(lengthArray[5].value==true){$("#hillsButton").html("Hills Unlocked")}
 }
 
 // Function to handle buying a length buyable (1 meter)
 function buyLengthBuyable(value) {
+    thisObject=lengthArray[value].valueOf();
     if (riderArray[0].gte(lengthArray[value].cost)==true) {
         riderArray[0] = riderArray[0].minus(lengthArray[value].cost); // Take away riders
         lengthArray[value].count = lengthArray[value].count.plus(new Decimal(1)); // Increment count
@@ -30,7 +28,7 @@ function buyLengthBuyable(value) {
     lengthArray[value].cost = newCost;
 }
 function buyLengthUpgrade(){
-    if(lengthArray[0].gte(new Decimal(250))==true){
+    if(lengthArray[0].gte(new Decimal(2500))==true){
         if(lengthArray[5].value==false){
             lengthArray[5].value=true;
 
@@ -41,11 +39,10 @@ function buyLengthUpgrade(){
 //for length page-specific saving
 
 // Update the game state every second
-window.setInterval(function(){
-    updateLength()
-    updateLengthBuyables()
+ window.setInterval(function(){
     incrementRiders();
     updateRiders();
+    updateLength();
     updateLengthBuyables();
 }, 1000);
 
--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function deleteSave(){
         lengthArray=[
                 new Decimal(0),//length(0)
                 //buyable 1 stuff(1)
-                {text1: "Press this to get 1 more meter of length<br>Requires: ",
+                {text1: "Press this to get 1 more meter of length<br>Costs: ",
                     text2: " riders<br>Bought: ",
                     count: new Decimal(0),
                     startCost: new Decimal(1),
@@ -40,7 +40,7 @@ function deleteSave(){
                     exponent: new Decimal(1.1),
                     adder: new Decimal(1)},
                     //buyable 2 stuff(2)
-                {text1: "Press this to get 2 more meters of length<br>Requires: ",
+                {text1: "Press this to get 2 more meters of length<br>Costs: ",
                     text2: " riders<br>Bought: ",
                     count: new Decimal(0),
                     startCost: new Decimal(4),
@@ -48,7 +48,7 @@ function deleteSave(){
                     exponent: new Decimal(1.2),
                     adder: new Decimal(2)},
                     //buyable 3 stuff(3)
-                {text1: "Press this to get 5 more meters of length<br>Requires: ",
+                {text1: "Press this to get 5 more meters of length<br>Costs: ",
                     text2: " riders<br>Bought: ",
                     count: new Decimal(0),
                     startCost: new Decimal(10),
@@ -60,12 +60,12 @@ function deleteSave(){
                     text2: " riders<br>Bought: ",
                     count: new Decimal(0),
                     startCost: new Decimal(20),
-                    cost: new Decimal(20),
+                    cost: new Decimal(25),
                     exponent: new Decimal(1.4),
                     adder: new Decimal(10)},
                     //upgrade stuff
                 {value:false,
-                    notBoughtText:"Press here to unlock hills<br>Requires 250 meters of length",
+                    notBoughtText:"Press here to unlock hills<br>Requires 2500 meters of length",
                     boughtText:"You have unlocked hills"},
             ];
 
