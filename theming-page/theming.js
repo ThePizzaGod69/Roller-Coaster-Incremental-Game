742883090be4a1b95199f2427932c5877249d075
ThePizzaGod69 <skeltonstevie@gmail.com>
2025-02-19 10:42:49 -0500

    Update theming.js

--- theming-page/theming.js
+++ theming-page/theming.js
@@ -60,7 +60,7 @@ function giveBuyableEffect(buyable){
     saveGame();
 }
 function buyEndUpgrade(){
-    if(themingArray[1].lt(new Decimal(ee100))){
+    if(themingArray[1].gte(Decimal.fromComponents(1,2,100))){
             themingArray[11]=true;
             saveGame();}
 
