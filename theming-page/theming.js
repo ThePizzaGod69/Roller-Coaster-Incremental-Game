ff07416354c73ba7923590b47d2e0e6e41a832f3
ThePizzaGod69 <skeltonstevie@gmail.com>
2025-02-19 10:57:11 -0500

    Update theming.js

--- theming-page/theming.js
+++ theming-page/theming.js
@@ -62,6 +62,7 @@ function giveBuyableEffect(buyable){
 function buyEndUpgrade(){
     if(themingArray[1].gte(Decimal.fromComponents(1,2,100))){
             themingArray[11]=true;
+            window.location.href = '../winner-page/winner-html';
             saveGame();}
 
         
